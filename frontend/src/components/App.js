bff2fa4f1b3749608f84e25a8fa547c8b5dc92ba
Roman PHILIP <romanphlp@gmail.com>
2021-11-30 18:50:48 +0100

    Commmit (ajout dependances, ajout banner, nav, et widget1)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Banner from './Banner'
+import Nav from './Nav'
+import Widget1 from './Widget1'
+import '../style/App.css'
 
 function App() {
-  return (
-    <div>
-      <Banner/>
-    </div>
-  )
+    return ( <
+        div >
+        <
+        Banner / >
+        <
+        Nav / >
+        <
+        div className = "widget_container" >
+        <
+        Widget1 / >
+        <
+        /div> <
+        /div>
+    )
 }
 
 export default App
