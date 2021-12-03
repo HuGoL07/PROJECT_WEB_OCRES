244d5003ad552f5bab33073c3d23a17b52613cbb
Hugo <hugo.leturgeon@gmail.com>
2021-12-03 09:30:13 +0100

    Ajout flexbox widget

--- frontend/src/component/Widget.js
+++ frontend/src/component/Widget.js
@@ -23,30 +23,30 @@ function Widget() {
             </div>
 
             {/*--- WIDGET 3 ---*/} 
-            <div className='widget3-container'>
-                <div className='widget3-banner'>
-                    <h2 className='widget3-title'> Ajouter une playlist </h2>
+            <div className='widget1-container'>
+                <div className='widget1-banner'>
+                    <h2 className='widget1-title'> Ajouter une playlist </h2>
                 </div>
             </div>
 
             {/*--- WIDGET 4 ---*/} 
-            <div className='widget4-container'>
-                <div className='widget4-banner'>
-                    <h2 className='widget4-title'> Supprimer un titre </h2>
+            <div className='widget2-container'>
+                <div className='widget2-banner'>
+                    <h2 className='widget2-title'> Supprimer un titre </h2>
                 </div>
             </div>
 
             {/*--- WIDGET 5 ---*/} 
-            <div className='widget5-container'>
-                <div className='widget5-banner'>
-                    <h2 className='widget5-title'> Vos titres </h2>
+            <div className='widget1-container'>
+                <div className='widget1-banner'>
+                    <h2 className='widget1-title'> Vos titres </h2>
                 </div>
             </div>
 
             {/*--- WIDGET 6 ---*/} 
-            <div className='widget6-container'>
-                <div className='widget6-banner'>
-                    <h2 className='widget6-title'> Notre API</h2>
+            <div className='widget2-container'>
+                <div className='widget2-banner'>
+                    <h2 className='widget2-title'> Notre API</h2>
                 </div>
             </div>
 
