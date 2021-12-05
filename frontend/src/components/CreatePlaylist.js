4b3c9695db3cc1512c28757675352ff09b646f28
Roman PHILIP <romanphlp@gmail.com>
2021-12-05 12:05:33 +0100

    Ajout widget Creer playlist: l'utilisateur rentre le nom + la decription et appuie sur le bouton "créer playlist"

--- frontend/src/components/CreatePlaylist.js
+++ frontend/src/components/CreatePlaylist.js
@@ -5,6 +5,7 @@ import { Container, Form } from 'react-bootstrap'
 import '../style/SearchWidget.css'
 import SpotifyWebApi from 'spotify-web-api-node'
 import TrackSearchResult from './TrackSearchResult'
+import { CleaningServices } from '@mui/icons-material'
 
 
 const spotifyApi = new SpotifyWebApi({
@@ -14,16 +15,27 @@ const spotifyApi = new SpotifyWebApi({
 export default function CreatePlaylist({ accessToken }) {
 
     let playlistId;
-    const [namePlaylist, setNamePlaylist] = useState("")
-        // const [descriptionPlaylist, setDescriptionPlaylist] = useState("")
 
+    //Initialisation du nom de la playlist avec du contenu vide
+    const [namePlaylist, setNamePlaylist] = useState("")
+    const [descriptionPlaylist, setDescriptionPlaylist] = useState("")
+
+    /// Va nous permettre d'attendre que l'utilisateur clique sur creer avant d'envoyer la requête 
+    const [wait, setWait] = useState(true)
+
+    //Des que la valeur de accessToken change, on re set la valeur avec setAccessToken
     useEffect(() => {
         if (!accessToken) return
         spotifyApi.setAccessToken(accessToken)
     }, [accessToken])
 
-    useEffect(() => {
-        spotifyApi.createPlaylist(namePlaylist)
+    ///Function qui prend en parametre le booléen wait 
+    ///Si wait === false, on demande à effectuer la requête avec en parametre 
+    /// nom playlist et description playlist, qu'on récupère plus bas dans le onChange()...
+
+    function sendRequest(wait) {
+        if (wait === false)
+            spotifyApi.createPlaylist(namePlaylist, { 'description': descriptionPlaylist })
             .then(res => {
                 console.log('Ok. Playlist created!');
                 playlistId = res.body['id'];
@@ -31,31 +43,56 @@ export default function CreatePlaylist({ accessToken }) {
 
             }, [accessToken]).catch(err => {
                 console.log(err)
+                console.log(descriptionPlaylist)
             })
-    })
+    }
 
-    return ( <
+    return (
+
+        <
         Container >
         <
         Form.Control type = "namePlaylist"
         placeholder = "Nom de la playlist que vous voulez créer"
         value = { namePlaylist }
-        onChange = { e => setNamePlaylist(e.target.value) }
+        onChange = {
+            e => {
+                setNamePlaylist(e.target.value)
+            }
+        }
         style = {
             { width: '50%' }
         }
-        />  
-
-        { /* <Form.Control type="descriptionPlaylist" value={ descriptionPlaylist } onChange = {e => setDescriptionPlaylist(e.target.value)}/> */ }
+        />   <
+        Form.Control type = "descriptionPlaylist"
+        placeholder = "Description de la playlist"
+        value = { descriptionPlaylist }
+        onChange = {
+            e => {
+                setDescriptionPlaylist(e.target.value)
+            }
+        }
+        style = {
+            { width: '50%' }
+        }
+        />          
 
         <
         div className = "login_button" >
 
         <
-        a className = "btn btn-success btn-lg" >
+        a className = "btn btn-success btn-lg"
+        onClick = {
+            () => {
+                ///Si on clique sur le bouton créer playlist, on met la valeur de wait à false et on 
+                /// appel la fonction qui effectue la requête avec wait en paramètres
+                setWait(false)
+                sendRequest(wait)
+            }
+        } >
         Creer playlist <
-        /a>  <
-        /div> <
+        /a>   <
+        /div>  <
         /Container>
     )
 
